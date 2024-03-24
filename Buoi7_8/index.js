8504153fc007880cb80b2de65fd89e55b75489be
minhthang1410 <minhthang14101998@gmail.com>
2024-03-24 22:15:12 +0700

    update buoi 7 va 8

---
+++ Buoi7_8/index.js
@@ -0,0 +1,42 @@
+// let a = 2;
+// let b = 3;
+// let res = a ** b;
+// console.log(res);
+// a--;
+// console.log(a);
+// a += 10; // a = a + 10
+// console.log(a);
+// let a = 10;
+
+// {
+//   // scope
+//   let a = 11;
+//   console.log(a);
+//   console.error("Đây là lỗi");
+//   console.warn("Đây là cảnh báo");
+// }
+
+// console.log(a);
+// let age = parseFloat(prompt("Nhập vào tuổi của bạn: "));
+// console.log(age);
+// console.log(typeof age);
+// let a = 5;
+// let b = 6;
+// console.log(a < b);
+// console.log(a > b);
+// console.log(a == b);
+// console.log("a" < "b");
+// let a = -10;
+
+// if (a > 0) {
+//   console.log(`${a} là số dương`);
+// } else if (a == 0) {
+//   console.log(`${a} là số 0`);
+// } else {
+//   console.log(`${a} là số âm`);
+// }
+let i = 0;
+while (i < 10) {
+  console.log(`${i} Hello`);
+  i++;
+}
